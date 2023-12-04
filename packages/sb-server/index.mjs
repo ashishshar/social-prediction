5dd4074c5647621237350a4b3abc1cccab6268b5
Ashish Sharma <ashish@shoppist.me>
2023-12-05 00:52:02 +0530

    fix

--- packages/sb-server/index.mjs
+++ packages/sb-server/index.mjs
@@ -15,20 +15,6 @@ import morgan from 'morgan';
 
 const app = express();
 connectDB();
-
-const isProduction = process.env.NODE_ENV === 'production';
-
-let server
-
-
-const options = {
-  key: fs.readFileSync(process.env.SSL_CRT_FILE),
-  cert: fs.readFileSync(process.env.SSL_KEY_FILE)
-};
-
-server = https.createServer(options, app);
-
-
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
@@ -40,7 +26,31 @@ app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3030;
 
-server.listen(PORT, () => {
-  console.log(`Server running on ${isProduction ? 'https' : 'http'}://localhost:${PORT}`);
-});
+if (process.env.HTTPS === 'true') {
+  let key;
+  let cert;
 
+  try {
+    key = fs.readFileSync(process.env.SSL_KEY_FILE, 'utf8');
+    cert = fs.readFileSync(process.env.SSL_CRT_FILE, 'utf8');
+  } catch (readError) {
+    console.error('Error reading SSL files:', readError.message);
+    process.exit(1); // Exit if there is an error
+  }
+
+  // Only proceed if both key and cert are successfully read
+  const options = { key, cert };
+
+  // Create an HTTPS server with the certificate and key
+  const httpsServer = https.createServer(options, app);
+
+  // Start the HTTPS server
+  httpsServer.listen(PORT, () => {
+    console.log('HTTPS Server running on port 3030');
+  });
+} else {
+  // Create a regular HTTP server if HTTPS is not enabled
+  app.listen(PORT, () => {
+    console.log('HTTP Server running on port 3030');
+  });
+}
