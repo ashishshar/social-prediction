0c81eb6828efbd0fcbe188c79a53bd3d879fedb7
Ashish Sharma <ashish@shoppist.me>
2023-12-05 00:31:05 +0530

    Update server

--- packages/sb-server/index.mjs
+++ packages/sb-server/index.mjs
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import http from 'http';  // Import the http module
+import https from 'https';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
@@ -7,8 +10,29 @@ import authRoutes from './src/routes/authRoutes.mjs';
 import connectDB from './src/config/database.mjs';
 import morgan from 'morgan';
 
+
+
+
 const app = express();
 connectDB();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+let server 
+
+if (isProduction) {
+  // In production, use HTTPS
+  const options = {
+    key: fs.readFileSync('./social-prediction.key'),
+    cert: fs.readFileSync('./social-prediction.key')
+  };
+  server = https.createServer(options, app);
+} else {
+  // In development, use HTTP
+  server = http.createServer(app);
+}
+
+
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
@@ -19,6 +43,8 @@ app.use('/api/transactions', transactionRoutes);
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3030;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+server.listen(PORT, () => {
+  console.log(`Server running on ${isProduction ? 'https' : 'http'}://localhost:${PORT}`);
 });
+
